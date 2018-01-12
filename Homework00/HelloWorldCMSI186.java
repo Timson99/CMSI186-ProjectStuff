c0c3599d299220f7e29ee8ded2e5a9ea9f850f91
Timson99 <34410019+Timson99@users.noreply.github.com>
2018-01-11 18:24:48 -0800

    Create HelloWorldCMSI186.java
    Adding file to Homework00 within the CMSI186-ProjectStuff Respiratory

---
+++ Homework00/HelloWorldCMSI186.java
@@ -0,0 +1,18 @@
+/** @HelloWorldCMSI186.java    1.0.0     2018-01-11  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  File          :  HelloWorldCMSI186.java
+ *  Purpose       :  Print "Hello, World!!!!!";
+ *  Author        :  Timothy Herrmann
+ *  Description   :  N/A
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  Revision History:
+ *  ------------------
+ *    Ver      Date     Modified by:  Description for change/modification
+ *   -----  ----------  ------------  -------------------------------------------------------------------
+ *   1.0.0  2018-01-11  T. Herrmann   Initial Version
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+public class HelloWorldCMSI186 {
+        public static void main(String[] args) {
+            System.out.println("\"Hello, World!!!!!\""); //Print "Hello, World!!!!!!"
+            }
+        } 
+     
