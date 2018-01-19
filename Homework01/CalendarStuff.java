e36e60cedf7d26f106f8ca86f57d1257e4146f4d
Timson99 <34410019+Timson99@users.noreply.github.com>
2018-01-19 00:48:59 -0800

    Revised and Comments Added

--- Homework01/CalendarStuff.java
+++ Homework01/CalendarStuff.java
@@ -18,7 +18,7 @@
 public class CalendarStuff {
     
   /**
-   * A listing of the days of the week, assigning numbers; Note that the week arbitrarily starts on Sunday
+   * A listing of the days of the week, assigning numbers; 
    */
    private static final int SUNDAY    = 0;
    private static final int MONDAY    = SUNDAY    + 1;
@@ -29,7 +29,7 @@ public class CalendarStuff {
    private static final int SATURDAY  = FRIDAY    + 1;
 
   /**
-   * A listing of the months of the year, assigning numbers; I suppose these could be ENUMs instead, but whatever
+   * A listing of the months of the year, assigning numbers; 
    */
    private static final int JANUARY    = 0;
    private static final int FEBRUARY   = JANUARY   + 1;
@@ -45,27 +45,25 @@ public class CalendarStuff {
    private static final int DECEMBER   = NOVEMBER  + 1;
   
   /**
-   * An array containing the number of days in each month
-   *  NOTE: this excludes leap years, so those will be handled as special cases
-   *  NOTE: this is optional, but suggested
+   * An array containing the number of days in each month.
    */
-   private static int[]    days        = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+   private static int[] days = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
   /**
    * The constructor for the class
    */
    public CalendarStuff() {
-      System.out.println( "Constructor was called" );  //Informs User if CalendarStuff class was constructed
+      System.out.println( "Constructor was called" );  //Tells User if CalendarStuff class was constructed
    }
 
   /**
-   * A method to determine if the year argument is a leap year or not<br />
+   * A method to determine if the year argument is a leap year or not
    *  Leap years are every four years, except for even-hundred years, except for every 400
    * @param    year  long containing four-digit year
    * @return         boolean which is true if the parameter is a leap year
    */
    public static boolean isLeapYear( long year ) {
-      return ((year%400 == 0) || (year%4 == 0) && !(year % 100 == 0));
+      return ((year%400 == 0) || (year%4 == 0) && !(year % 100 == 0)); //Are these magic numbers?
    }
 
   /**
@@ -73,13 +71,10 @@ public class CalendarStuff {
    * @param    month long containing month number, starting with "1" for "January"
    * @param    year  long containing four-digit year; required to handle Feb 29th
    * @return         long containing number of days in referenced month of the year
-   * notes: remember that the month variable is used as an indix, and so must
-   *         be decremented to make the appropriate index value
    */
    public static long daysInMonth( long month, long year ) {
-       
-        if(isLeapYear(year) && (month == 2)) {
-            return (days[(int)month - 1] + 1);
+        if(isLeapYear(year) && (month - 1 == FEBRUARY)) { //Checks if month in question is a February during a Leap Year
+            return (days[(int)month - 1] + 1);            //Returns days in a regular February plus 1, to account for a leap day
         }
         else
             return (days[(int)month - 1]);
@@ -95,9 +90,8 @@ public class CalendarStuff {
    * @param    year2  long    containing four-digit year
    * @return          boolean which is true if the two dates are exactly the same
    */
-   public static boolean dateEquals( long month1, long day1, long year1, long month2, long day2, long year2 ) {
-        
-        return ((year1 == year2) && (month1 == month2) && (day1 == day2));
+   public static boolean dateEquals( long month1, long day1, long year1, long month2, long day2, long year2 ) { 
+        return ((year1 == year2) && (month1 == month2) && (day1 == day2)); //Returns true or false depending on whether or not  the two months, days, and years are both equal
    }
 
   /**
@@ -111,7 +105,6 @@ public class CalendarStuff {
    * @return          int    -1/0/+1 if first date is less than/equal to/greater than second
    */
    public static int compareDate( long month1, long day1, long year1, long month2, long day2, long year2 ) {
-      
       boolean yearGreaterOrEqual = year1 >= year2;
       boolean monthGreaterOrEqual = month1 >= month2;
       
@@ -122,6 +115,7 @@ public class CalendarStuff {
       else 
           return -1;
     }
+    
   /**
    * A method to return whether a date is a valid date
    * @param    month long    containing month number, starting with "1" for "January"
@@ -132,16 +126,12 @@ public class CalendarStuff {
    *         be decremented to make the appropriate index value
    */
    public static boolean isValidDate( long month, long day, long year ) {
-        
-        if(isLeapYear(year) == true && (day != 1))
-            day--;
-        
-        if((year >= 0) && (month >= 1 && month <= 12) && ( (day >= 1) && day <= days[ (int)(month - 1) ] ))
+        if(isLeapYear(year) == true && (month - 1 == FEBRUARY) && (day == days[FEBRUARY] + 1)) //Variable day is a valid leap day if the date passed in is the 29th day of February during a leap year
+            day--; //Subtracts 1 from day if the variable day happens to be a leap day, allowing it to be checked for validity based on the regular days in the month of February
+        if((year >= 0) && (month - 1 >= JANUARY && month - 1 <= DECEMBER) && ( (day >= 1) && day <= days[ (int)(month - 1) ] ))
             return true;
-        
         else 
             return false;
-        
    }
 
   /**
@@ -150,7 +140,6 @@ public class CalendarStuff {
    * @return         String containing the string value of the month (no spaces)
    */
    public static String toMonthString( int month ) {
- 
       switch( month - 1 ) {
          case 0: return "January";
          case 1: return "February";
@@ -174,7 +163,6 @@ public class CalendarStuff {
    * @return       String containing the string value of the day (no spaces)
    */
    public static String toDayOfWeekString( int day ) {
-      
       switch( day - 1 ) {
          case 0: return "Sunday";
          case 1: return "Monday";
@@ -186,8 +174,6 @@ public class CalendarStuff {
          default: throw new IllegalArgumentException( "Illegal day value given to 'toDayOfWeekString()'." );
       }
    }
-
-   
 
   /**
    * A method to return a count of the total number of days between two valid dates
@@ -203,8 +189,11 @@ public class CalendarStuff {
         long dayCount = 0;
         long[] greaterDate;
         long[] lesserDate;
-        long[] markedDate; 
+        long[] markedDate; //Array meant to hold the date's month and day that furthest through the calendar, regardless of year.
      
+        /* -This switch statement determines which overall date is larger. 
+          *  -Statement initializes lesserDate and greaterDate with the appropriate date
+          */
         switch(compareDate(month1, day1, year1, month2, day2, year2)) {
             case 0: 
                 return 0;
@@ -223,30 +212,41 @@ public class CalendarStuff {
                 break;
         }
         
+        /*  -Checks to see if the two dates have equal months and unequal days
+          *  -If so, the positive difference is added to the dayCount variable 
+          */
         if(greaterDate[1] != lesserDate[1] && greaterDate[0] == lesserDate[0]) {
            
             dayCount += greaterDate[1] - lesserDate[1];
-            markedDate = new long[] {greaterDate[0], greaterDate[1]};
+            markedDate = new long[] {greaterDate[0], greaterDate[1]}; //-Since months match, the markedDate is the date with the higher day number
         }
+        /*  -
+          *  -
+          *  -
+          */
         else {
+            /*  -
+                *  - 
+                *  -
+                */
             if(greaterDate[0] > lesserDate[0]) {
-                
                 long monthCounter = lesserDate[0];
                 long monthDifference = greaterDate[0] - lesserDate[0];
                 dayCount += daysInMonth(lesserDate[0], lesserDate[2]) - lesserDate[1];
                 monthCounter++;
                 
                 for(int m = 0; m < monthDifference - 1; m++) {
-                    
                     dayCount += daysInMonth(monthCounter, lesserDate[2]);
                     monthCounter++;
                 }
                 markedDate = new long[] {greaterDate[0], greaterDate[1]};
                 dayCount += greaterDate[1];
             }
-        
+            /*  -
+               *  -
+               *  -
+               */
             if(greaterDate[0] < lesserDate[0]) {
-                
                 long monthCounter = greaterDate[0];
                 long monthDifference = lesserDate[0] - greaterDate[0];
                 dayCount -= daysInMonth(greaterDate[0], greaterDate[2]) - greaterDate[1];
@@ -262,23 +262,32 @@ public class CalendarStuff {
             }
             else
                 markedDate = new long[] {greaterDate[0], greaterDate[1]};
-           
         }
-        if(greaterDate[2] != lesserDate[2]) {
-            
+        /*  -
+          *  -
+          *  -
+          */
+        if(greaterDate[2] != lesserDate[2]) {            
             int yearDifference = (int)(greaterDate[2] - lesserDate[2]);
+            
+            /*  -
+               *  -
+               *  -
+               */
             if(compareDate(markedDate[0], markedDate[1], greaterDate[2], FEBRUARY, days[FEBRUARY], greaterDate[2]) == -1) {
                 for(int y = 0; y < yearDifference; y++) {
-                
                     if (isLeapYear(lesserDate[2] + y))
                         dayCount += 366;
                     else
                         dayCount += 365;
                 }
             }
+            /*  -
+               *  -
+               *  -
+               */
             else {
                 for(int y = 0; y < yearDifference; y++) {
-                
                     if (isLeapYear(lesserDate[2] + 1 + y))
                         dayCount += 366;
                     else
